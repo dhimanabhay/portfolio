bdcdd9e83124711cd4a67098c245cced8ada0aca
adhiman <adhiman@sfu.ca>
2022-12-12 23:22:06 -0800

    routes

---
+++ src/components/Front.js
@@ -0,0 +1,28 @@
+import "./Front.css";
+
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+const Front = () => {
+  return (
+    <div className="front">
+        <div className="mask">
+            <div className="content">
+                <p>
+                    Meet me,
+                </p>
+                <h1>
+                    Abhay Dhiman
+                </h1>
+                <div>
+                    <Link to="/project" className="btn">PROJECTS</Link>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Front
